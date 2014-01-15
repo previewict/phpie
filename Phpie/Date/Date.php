8ce267492292468aeee2a54d0f8ebcbf4ae1db74
Shaharia Azam <shaharia.azam@gmail.com>
2014-01-15 20:54:08 +0600

    Few method written for Date component

--- Phpie/Date/Date.php
+++ Phpie/Date/Date.php
@@ -11,9 +11,97 @@ namespace Phpie\Date;
 
 class Date
 {
-    public function test($arg = null)
+    public $timezone = 'Asia/Dhaka';
+
+    public $format;
+
+    public static $defaultFormat = 'm/d/y h:i:s';
+
+    /**
+     * Pre-defined global format constant for datetime format
+     * @ref http://www.php.net/manual/en/class.datetime.php#datetime.synopsis
+     * @var array
+     */
+    public static $formatConst = array(
+        'ATOM' => "Y-m-d\TH:i:sP",
+        'COOKIE' => "l, d-M-y H:i:s T",
+        'ISO8601' => "Y-m-d\TH:i:sO",
+        'RFC822' => "D, d M y H:i:s O",
+        'RFC850' => "l, d-M-y H:i:s T",
+        'RFC1036' => "D, d M y H:i:s O",
+        'RFC1123' => "D, d M Y H:i:s O",
+        'RFC2822' => "D, d M Y H:i:s O",
+        'RFC3339' => "Y-m-d\TH:i:sP",
+        'RSS' => "D, d M Y H:i:s O",
+        'W3C' => "Y-m-d\TH:i:sP"
+    );
+
+    function __construct($timezone = null)
     {
-        return $arg;
+        //To avoid warnings, we set timezone with our variable $timezone
+        self::setTimezone($this->timezone);
+
+        if(isset($timezone)){
+            self::setTimezone($timezone);
+        }
+
+        $this->setFormat(self::$defaultFormat);
     }
 
+    public function getFormat()
+    {
+        if(!isset($this->format)){
+            $this->format = self::$defaultFormat;
+        }
+        return $this->format;
+    }
+
+    public function setFormat($format = null)
+    {
+        if(empty($format)){
+            $this->format = self::$defaultFormat;
+        }
+        if(array_key_exists($format, self::$formatConst)){
+            $this->format = self::$formatConst[$format];
+        }else{
+            $this->format = $format;
+        }
+    }
+
+    public function setTimezone($timezone)
+    {
+        if(self::isValidTimezone($timezone) === true){
+            $this->timezone = $timezone;
+        }
+        date_default_timezone_set($this->timezone);
+    }
+
+    public function getTimezone()
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * Timezone list with GMT offset
+     * @return array
+     */
+    public static function timezoneList() {
+        $timestamp = time();
+        $zoneList = array();
+        foreach(timezone_identifiers_list() as $key => $zone) {
+            date_default_timezone_set($zone);
+            $zoneList[$key]['zone_name'] = $zone;
+            $zoneList[$key]['gmt_offset'] = 'UTC/GMT ' . date('P', $timestamp);
+        }
+        return $zoneList;
+    }
+
+    /**
+     * @param $timezone
+     * @return bool
+     */
+    public function isValidTimezone($timezone) {
+        $timezoneList = timezone_identifiers_list();
+        return (bool)in_array($timezone, $timezoneList);
+    }
 } 
--- Tests/Phpie/Date/DateTest.php
+++ Tests/Phpie/Date/DateTest.php
@@ -1,18 +1,78 @@
 <?php
 include_once('../Bootstrap.php');
 
-use Phpie\Date;
+use Phpie\Date\Date;
 
 class DateTest extends PHPUnit_Framework_TestCase {
 
     public function setUp()
     {
-        $this->Date = new Phpie\Date\Date();
+        $this->Date = new Date();
     }
 
-    public function testWithoutArgument()
+    public function testClassLoadWithoutTimezone()
     {
-        $this->assertEquals(NULL, $this->Date->test());
+        $this->Date = new Date();
+    }
+
+    public function testClassLoadWithTimezone()
+    {
+        $this->Date = new Date('Australia/AoCT');
+        $this->assertEquals('Asia/Dhaka',$this->Date->getTimezone());
+    }
+
+    public function testClassLoadWithInvalidTimezone()
+    {
+        $this->Date = new Date('Invalid Timezone');
+        $this->assertEquals('Asia/Dhaka',$this->Date->getTimezone());
+    }
+
+    public function testClassLoadWithValidTimezone()
+    {
+        $this->Date = new Date('Australia/Sydney');
+        $this->assertEquals('Australia/Sydney',$this->Date->getTimezone());
+    }
+
+    public function testSetTimezoneWithValidTimezone()
+    {
+        $this->Date->setTimezone('Australia/Sydney');
+        $this->assertEquals('Australia/Sydney', $this->Date->getTimezone());
+    }
+
+    public function testSetTimezoneWithInvalidTimezone()
+    {
+        $this->assertEmpty($this->Date->setTimezone('Invalid Timezone'));
+    }
+
+    public function testGetTimezoneList()
+    {
+        $this->assertNotEmpty($this->Date->timezoneList());
+    }
+
+
+
+    public function testTimezoneIdValidationForValidTimezone()
+    {
+        $this->assertTrue(true, $this->Date->isValidTimezone('Asia/Dhaka'));
+    }
+
+    public function testTimezoneIdValidationForInvalidTimezone()
+    {
+        $this->assertFalse(false, $this->Date->isValidTimezone('Invalid Timezone'));
+    }
+
+    public function testSetFormat()
+    {
+        $this->Date->setFormat('m/d/y');
+        $this->assertEquals('m/d/y', $this->Date->getFormat());
+    }
+
+    public function testSetFormatForPredefinedFormatConstant()
+    {
+        foreach(Date::$formatConst as $key => $value){
+            $this->Date->setFormat(Date::$formatConst[$key]);
+            $this->assertEquals($value, $this->Date->getFormat());
+        }
     }
 }
  
