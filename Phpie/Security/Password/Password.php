52d60f9c82d53353b669de837a0c921b869a9120
Shaharia Azam <shaharia.azam@gmail.com>
2015-01-26 14:47:56 +0600

    Password generate and verify method updated

--- Phpie/Security/Password/Password.php
+++ Phpie/Security/Password/Password.php
@@ -8,8 +8,26 @@ namespace Phpie\Security\Password;
 
 class Password
 {
-    public function generate($password)
+    /**
+     * @param $password
+     * @return bool|false|string
+     */
+    public static function generate($password)
     {
         return password_hash($password, PASSWORD_BCRYPT);
+    }
+
+    /**
+     * @param $password
+     * @param $hash
+     * @return bool
+     */
+    public static function verify($password, $hash)
+    {
+        if (password_verify($password, $hash)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
